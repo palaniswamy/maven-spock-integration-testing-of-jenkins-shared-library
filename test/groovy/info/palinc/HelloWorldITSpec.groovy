33b985ce160fe5ca49b82ff6814a0f1f09b67e61
Pal Ubuntu20.04 <pal@pal-Latitude-E7450>
2020-05-12 17:36:43 -0400

    Mock of an Integration Test using Failsafe plugin

---
+++ test/groovy/info/palinc/HelloWorldITSpec.groovy
@@ -0,0 +1,23 @@
+package info.palinc
+
+import spock.lang.Specification
+
+class HelloWorldITSpec extends Specification {
+
+    def "runMethod() returns success"() {
+        given:
+        def script = [
+                echo: Mock(JenkinsStep)
+        ]
+        def helloWorld = new HelloWorld(script)
+
+        when:
+        def result = helloWorld.runMethod()
+
+        then:
+        1 * script.echo.call("Hello World")
+        result == 'success'
+    }
+
+
+}
